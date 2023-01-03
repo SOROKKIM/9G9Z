e43e8be55494d49fc148ac0cc6cbfb39ad9591d7
arkt0956 <55053131+arkt0956@users.noreply.github.com>
2023-01-03 10:56:57 +0900

    Fix: HeartService, HeartController
    1. board -> post 네이밍 변경
    2. postRepository 삭제(HeartService)
    3. comment, post likes 필드 수정자 삭제(HeartService)
    4. URL 수정(HeartController)

--- src/main/java/com/sparta/moviecomunnity/service/HeartService.java
+++ src/main/java/com/sparta/moviecomunnity/service/HeartService.java
@@ -25,24 +25,19 @@ import static com.sparta.moviecomunnity.exception.ErrorCode.MEMBER_NOT_FOUND;
 public class HeartService {
     private final UserRepository userRepository;
     private final HeartRepository heartRepository;
-    private final PostRepository boardRepository;
     private final CommentRepository commentRepository;
 
-    public HttpResponseDto addBoardLikes(Long boardId, String subject) {
+    public HttpResponseDto updatePostLikes(Long boardId, String subject) {
         User user = userRepository.findByUsername(subject).orElseThrow(
                 () -> new CustomException(MEMBER_NOT_FOUND)
         );
 
         Optional<Heart> heart = heartRepository.findHeartByUserAndBoardId(user, boardId);
-        Board board = boardRepository.findById(boardId).get();
 
         if(heart.isPresent()) {
-            board.minusLikes();
-            boardRepository.save(board);
             heartRepository.deleteById(heart.get().getId());
         }
         else {
-            board.addLikes();
             heartRepository.save(new Heart(user, boardId));
         }
 
@@ -50,22 +45,19 @@ public class HeartService {
     }
 
 
-    public HttpResponseDto addCommentLikes(Long commentId, String subject) {
+    public HttpResponseDto updateCommentLikes(Long commentId, String subject) {
         User user = userRepository.findByUsername(subject).orElseThrow(
                 () -> new CustomException(MEMBER_NOT_FOUND)
         );
 
         Optional<Heart> heart = heartRepository.findHeartByUserAndCommentId(user, commentId);
-        Comment comment = commentRepository.findById(commentId).get();
+        Comment comment = commentRepository.findById(commentId).get(); // 클라이언트는 클릭으로 좋아요 하기 때문에 없는 댓글에 좋아요를 할 수 없다. null일때의 예외처리 x
 
         if(heart.isPresent()) {
-            comment.minusLikes();
-            commentRepository.save(comment);
             heartRepository.deleteById(heart.get().getId());
         }
         else {
-            comment.addLikes();
-            heartRepository.save(new Heart(user, comment.getBoard().getId(), comment.getId()));
+            heartRepository.save(new Heart(user, comment.getPostId(), commentId));
         }
 
         return new HttpResponseDto("success", 200L);
