b699b6dc3c18ad85cc7f36db1aea0e6fd29537a5
beom <baek940dog@gmail.com>
2023-01-04 17:17:17 +0900

    Fix: SecurityFilter: config 파일 수정

--- src/main/java/com/sparta/moviecomunnity/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/moviecomunnity/config/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig {
         // 기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        http.authorizeRequests().antMatchers("/movies/user/**").permitAll()
+        http.authorizeRequests().antMatchers("/movies/users/**").permitAll()
                 .antMatchers(HttpMethod.GET,"/movies/posts").permitAll()
                 .anyRequest().authenticated()
                 // JWT 인증/인가를 사용하기 위한 설정
